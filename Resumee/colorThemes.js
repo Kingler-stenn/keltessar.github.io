94a82b2f92cf90e6590c33866584fa503de5f92d
keltessar <keltessar@gmail.com>
2020-10-22 23:01:35 +0700

    First Exp with git pages

---
+++ Resumee/colorThemes.js
@@ -0,0 +1,34 @@
+let theme = localStorage.getItem('theme')
+
+if (theme == null) {
+    setTheme('light');
+} else {
+    setTheme(theme)
+}
+
+
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+for (var i=0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function() {
+        let mode = this.dataset.mode;
+        console.log('option clicked', mode);
+        setTheme(mode);
+    })
+
+}
+
+function setTheme(mode) {
+    if(mode == 'light') {
+        document.getElementById('theme-style').href = 'styles.css'
+    }
+    if(mode == 'dark') {
+        document.getElementById('theme-style').href = 'darkDot.css'
+    }
+    if(mode == 'purple') {
+        document.getElementById('theme-style').href = 'purpleDot.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
